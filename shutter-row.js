18dd362437ae4a281c9904e096df164320373065
Niklas Schumacher <sniklas96@gmail.com>
2022-09-11 16:47:25 +0200

    Use configured entity icon (#3); Added 'state_color' to configuration, switches on colored state icon (#7)

--- shutter-row.js
+++ shutter-row.js
@@ -155,15 +155,16 @@ div.controls[state="unavailable"] ha-icon  {
     color: var(--disabled-text-color);
     cursor: default;
 }
-.ghost {
-    display: none !important;
-}
 ha-card {
     padding: 16px;
 }
 ha-icon[disabled] {
     color: var(--disabled-text-color);
     cursor: default;
+}
+ha-icon.active-icon {
+    color: #fdd835 !important;
+    color: var(--paper-item-icon-active-color, #fdd835) !important;
 }
 div.card-row {
     display: flex;
@@ -207,28 +208,18 @@ let VERSION = "0.1.1";
 
 
 class ShutterRow extends s {
+    /*
+        Lovelace needed functions
+    */
     static get properties() {
         return {
             hass: Object,
             config: Object,
         };
     }
-
-    static get styles() {
-        return css_248z;
-    }
-
-    getConfig() {
-        let getConfigAttribute = (attribute, defaultValue) => {
-            return attribute in this._config ? this._config[attribute] : defaultValue;
-        };
-
-        return {
-            type: getConfigAttribute("type", false),
-            entity: getConfigAttribute("entity", false),
-            name: getConfigAttribute("name", false),
-            invert_position: getConfigAttribute("invert_position", false),
-        };
+    // Lovelace card height
+    getCardSize() {
+        return 2;
     }
     // On user config update
     setConfig(config) {
@@ -248,19 +239,38 @@ class ShutterRow extends s {
         };
         this.entityId = this.config.entity;
     }
-  
-    // Lovelace card height
-    getCardSize() {
-        return 2;
+
+    /*
+        Card functions
+    */
+    // Returns CSS style
+    static get styles() {
+        return css_248z;
     }
+    // Returns complete user config
+    getConfig() {
+        let getConfigAttribute = (attribute, defaultValue) => {
+            return attribute in this._config ? this._config[attribute] : defaultValue;
+        };
+        return {
+            type: getConfigAttribute("type", false),
+            entity: getConfigAttribute("entity", false),
+            name: getConfigAttribute("name", false),
+            invert_position: getConfigAttribute("invert_position", false),
+            state_color: getConfigAttribute("state_color", false)
+        };
+    }
+    // Get entity name
     getName() {
         if(this.getConfig().name)
             return this.getConfig().name;
         return this.state.attributes.friendly_name;
     }
+    // Get position value
     getPosition() {
         return this.state.attributes.current_position;
     }
+    // Get position label
     getPositionDisplay() {
         if( (this.getConfig().invert_position && this.getPosition() == 100) ||
                 (!this.getConfig().invert_position && this.getPosition() == 0) )
@@ -270,6 +280,7 @@ class ShutterRow extends s {
             return this.hass.localize("component.cover.state._.open");
         return `${this.getPosition()} %`;
     }
+    // Sets meta for variables
     setMeta(force=false) {
         // Only on change
         if(this.state == this.hass.states[this.entityId] && !force)
@@ -277,38 +288,50 @@ class ShutterRow extends s {
         this.state = this.hass.states[this.entityId];
         this.stateDisplay = this.state ? this.state.state : 'unavailable';
     }
+    upReached() {
+        if(!this.getConfig().invert_position && this.getPosition() == 100 ||
+        this.getConfig().invert_position && this.getPosition() == 0)
+                return true;
+        return false;
+    }
+    downReached() {
+        if(this.getConfig().invert_position && this.getPosition() == 100 ||
+            !this.getConfig().invert_position && this.getPosition() == 0)
+                return true;
+        return false;
+    }
 
+    // Render entity icon
+    renderIcon() {
+        let icon = "";
+        // Check for entity defined icon
+        if(this.state.attributes.icon != undefined)
+            icon = this.state.attributes.icon;
+        else {
+            // Use dynamic icon
+            if(this.downReached())
+                icon = "mdi:window-shutter";
+            else
+                icon = "mdi:window-shutter-open";
+        }
+        return y`<ha-icon icon="${icon}" class="${this.getConfig().state_color != undefined && this.getConfig().state_color ? "active-icon" : ""}"></ha-icon>`;
+    }
+    // Render lovelace card
     render() {
         this.setMeta();
 
-        let config = this.getConfig();
-        let position = this.getPosition();
-
-        let upReached = function() {
-            if(!config.invert_position && position == 100 ||
-                    config.invert_position && position == 0)
-                    return true;
-            return false;
-        };
-        let downReached = function() {
-            if(config.invert_position && position == 100 ||
-                !config.invert_position && position == 0)
-                    return true;
-            return false;
-        };
         return y`
             <ha-card>
                 <div class="card-row card-first-row">
                     <div class="entity-icon">
-                        <ha-icon icon="mdi:window-shutter-open" class="${downReached() ? 'ghost' : ''}"></ha-icon>
-                        <ha-icon icon="mdi:window-shutter" class="${!downReached() ? 'ghost' : ''}"></ha-icon>
+                        ${this.renderIcon()}
                     </div>
                     
                     <span class="entity-name" @click="${this.moreInfo}">${this.getName()}</span>
                     <div class="controls" state="${this.stateDisplay}">
-                        <ha-icon icon="mdi:chevron-up" class="${upReached() || this.state.attributes.moving == "UP" ? "disabled": ''}" @click="${this.onUpClick}"></ha-icon>
-                        <ha-icon icon="mdi:stop" class="${this.state.attributes.moving == "STOP" ? "disabled": ''}" @click="${this.onStopClick}"></ha-icon>
-                        <ha-icon icon="mdi:chevron-down" class="${downReached() || this.state.attributes.moving == "DOWN" ? "disabled": ''}" @click="${this.onDownClick}"></ha-icon>
+                        <ha-icon icon="mdi:chevron-up" class="${this.upReached() || this.state.attributes.moving == "UP" ? "disabled" : ''}" @click="${this.onUpClick}"></ha-icon>
+                        <ha-icon icon="mdi:stop" class="${this.state.attributes.moving == "STOP" ? "disabled" : ''}" @click="${this.onStopClick}"></ha-icon>
+                        <ha-icon icon="mdi:chevron-down" class="${this.downReached() || this.state.attributes.moving == "DOWN" ? "disabled" : ''}" @click="${this.onDownClick}"></ha-icon>
                     </div>
                 </div>
                 <div class="card-row card-second-row">
@@ -320,31 +343,36 @@ class ShutterRow extends s {
             </ha-card>
         `;
     }
-    getElements() {
+
+    /*
+        DOM element handler
+    */
+   // Get all important DOM elements
+    _getElements() {
         return {
             controls: this.renderRoot.querySelector("div.card-first-row div.controls"),
             slider: this.renderRoot.querySelector("div.card-second-row ha-slider"),
         }
     }
-
+    // On move up button click
     onUpClick() {
-        let elements = this.getElements();
+        let elements = this._getElements();
         if(this.state.attributes.moving == "UP" || elements.controls.hasAttribute("up-reached"))
             return;
         this.hass.callService("cover", "open_cover", {
             entity_id: this.entityId,
         });
     }
-
+    // On move down button click
     onDownClick() {
-        let elements = this.getElements();
+        let elements = this._getElements();
         if(this.state.attributes.moving == "DOWN" || elements.controls.hasAttribute("down-reached"))
             return;
         this.hass.callService("cover", "close_cover", {
             entity_id: this.entityId,
         });
     }
-
+    // On stop button click
     onStopClick() {
         if(this.state.attributes.moving == "STOP")
             return;
@@ -352,9 +380,9 @@ class ShutterRow extends s {
             entity_id: this.entityId,
         });
     }
-
+    // On position input change
     onSliderChange() {
-        let elements = this.getElements();
+        let elements = this._getElements();
         if(elements.slider.value == this.getPosition())
             return;
         this.hass.callService("cover", "set_cover_position", {
@@ -362,7 +390,7 @@ class ShutterRow extends s {
             position: parseInt(elements.slider.value),
         });
     }
-
+    // Open HA more info
     moreInfo() {
         let entityId = this.getConfig().entity;
         ne(this, 'hass-more-info', {
